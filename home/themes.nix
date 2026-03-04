6bcd439ab1ad2d2a6f0a0c35de2fc632f930569f
Visut Hsu <visuthsu@gmail.com>
2026-03-04 22:31:18 +0700

    generation 145

--- home/themes.nix
+++ home/themes.nix
@@ -1,17 +1,4 @@
-{
-  pkgs,
-  stable-pkgs,
-  ...
-}: let
-  qt_config = {
-    Appearance = {
-      style = "Darkly";
-      color_scheme_path = "/home/numblr/.nix-profile/share/qt5ct/colors/catppuccin-macchiato-blue.conf";
-      custom_palette = "true";
-      standar_dialogs = "default";
-    };
-  };
-in {
+{pkgs, ...}: {
   home.pointerCursor = {
     enable = true;
     package = pkgs.bibata-cursors;
@@ -24,14 +11,7 @@ in {
 
   qt = {
     enable = true;
-    platformTheme.name = "qtct";
-    style.package = with pkgs; [
-      stable-pkgs.darkly-qt5
-      stable-pkgs.darkly
-      catppuccin-qt5ct
-    ];
-    qt5ctSettings = qt_config;
-    qt6ctSettings = qt_config;
+    platformTheme.name = "gtk3";
   };
 
   gtk = {
