9557dd10af27e0780a55f3b160cba7ebb4618c24
Visut Hsu <visuthsu@gmail.com>
2026-03-11 21:30:14 +0700

    generation 162

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -1,9 +1,10 @@
 let
+  wallpaper = ./configs/hyprland/wallpaper.png;
   configDir = ./configs/hyprland;
   entries = builtins.readDir configDir;
   configfiles = builtins.sort builtins.lessThan (
     builtins.filter
-    (name: entries.${name} == "regular")
+    (name: builtins.match "\\.conf$" name != null)
     (builtins.attrNames entries)
   );
 in {
@@ -12,5 +13,26 @@ in {
     enable = true;
     systemd.enable = false;
     extraConfig = builtins.concatStringsSep "\n" (map (name: "source = ${configDir}/${name}") configfiles);
+  };
+
+  programs.vicinae = {
+    enable = true;
+    settings.launcher_window.layer_shell.enabled = false;
+    systemd = {
+      enable = true;
+      autoStart = true;
+    };
+  };
+
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      ipc = "on";
+      splash = false;
+      splash_offset = 2.0;
+
+      preload = ["${wallpaper}"];
+      wallpaper = [",${wallpaper}"];
+    };
   };
 }
